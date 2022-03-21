e1773ba80f3e0a2cc7864df29389377a3926f24c
hello <hello@wor.ld>
2022-03-21 16:17:03 +0530

    Added API URL

---
+++ SwiftUIAssignment/Common/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SwiftUIAssignment
+//
+//  Created by Hello World on 21/03/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiUrl = "https://jsonplaceholder.typicode.com/photos"
+}
