578d95cd6edac3114dd7c0bd9b213b579a2f49fb
hello <hello@wor.ld>
2022-03-21 15:45:18 +0530

    Initial Commit

---
+++ SwiftUIAssignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAssignment
+//
+//  Created by Hello World on 21/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAssignment/SwiftUIAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAssignmentApp.swift
+//  SwiftUIAssignment
+//
+//  Created by Hello World on 21/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
